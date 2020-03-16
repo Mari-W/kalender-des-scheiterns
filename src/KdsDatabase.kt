208d06926161d3d59e8d777f8b8fd6a44cd29331
moritz <git@moehritz.de>
2020-03-17 00:16:42 +0100

    add database stuff

---
+++ src/KdsDatabase.kt
@@ -0,0 +1,16 @@
+package de.moeri
+
+import org.sql2o.Sql2o
+import java.sql.Date
+
+val db: Sql2o = Sql2o("jdbc:mysql://78.47.156.57:3306/kds", "root", "moeriland42")
+
+fun insertIdea(idea: Idea) {
+
+}
+
+fun listIdeas(): List<Idea> {
+    return listOf(Idea("DIE PARTEI wurde gegründet", Date.valueOf("02/08/2004"), "https://www.google.com/search?q=die+partei+gr%C3%BCndung"))
+}
+
+data class Idea(val idea: String, val date: Date, val source: String)
