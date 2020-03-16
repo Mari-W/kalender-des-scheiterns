16af85bd190af40f2069884061f9ae41aca0756c
moritz <git@moehritz.de>
2020-03-17 00:53:32 +0100

    check

--- src/KdsDatabase.kt
+++ src/KdsDatabase.kt
@@ -2,7 +2,9 @@ package de.moeri
 
 import org.sql2o.Sql2o
 import java.sql.Date
+import java.util.regex.Pattern
 
+val urlCheck = Pattern.compile("(https?://(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?://(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})")
 val db: Sql2o = Sql2o("jdbc:mysql://78.47.156.57:3306/kds", "root", "moeriland42")
 
 fun insertIdea(idea: Idea) {
@@ -18,6 +20,22 @@ fun listIdeas(): List<Idea> {
         return it.createQuery("SELECT * FROM kds_idea")
             .executeAndFetch(Idea::class.java)
     }
+}
+
+fun controlIdea(idea: Idea): Boolean {
+    val ideaLen = idea.idea.length
+    if (5 <= ideaLen || ideaLen <= 420) {
+        println("idea to long/short, $ideaLen")
+        return false
+    }
+
+    if (!urlCheck.matcher(idea.source).matches()) {
+        println("url wrong, ${idea.source}")
+        return false
+    }
+
+
+    return true
 }
 
 data class Idea(val idea: String, val date: Date, val source: String, val id: Int = 0)
