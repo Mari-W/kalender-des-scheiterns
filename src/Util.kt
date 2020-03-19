bd23b4c118c34efecd739ab7e70bafd86fa24cfb
Mari <git@mariusweidner.de>
2020-03-19 18:18:47 +0100

    PAPER PLANE

--- src/Util.kt
+++ src/Util.kt
@@ -1,5 +1,10 @@
 package de.moeri
 
+import io.ktor.application.ApplicationCall
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import org.jtwig.JtwigModel
+import org.jtwig.JtwigTemplate
 import java.io.File
 import java.security.MessageDigest
 
@@ -34,4 +39,19 @@ fun File.ensureItExists(): File {
     if (!exists())
         mkdir()
     return this
+}
+
+private val templates = mutableMapOf<String, JtwigTemplate>()
+
+suspend fun ApplicationCall.respondTwig(template: String, model: Map<String, Any> = mapOf()) {
+    if (!templates.containsKey(template))
+        templates[template] = JtwigTemplate.classpathTemplate("template/$template.twig")
+    val twigModel = JtwigModel.newModel()
+    model.forEach { (key, value) ->
+        twigModel.with(key, value)
+    }
+    respondText(
+        templates[template]!!.render(twigModel),
+        ContentType.Text.Html
+    )
 }
