2ae3e136e2c761c2b4d7b00ccf60ab2e5ccc0e0f
Mari <git@mariusweidner.de>
2020-03-19 14:21:24 +0100

    PAPER PLANE

--- src/Application.kt
+++ src/Application.kt
@@ -11,8 +11,15 @@ import io.ktor.http.ContentType
 import org.slf4j.event.*
 import io.ktor.http.HttpStatusCode
 import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import org.jtwig.JtwigModel
 import org.jtwig.JtwigTemplate
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 import java.sql.Date
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -46,7 +53,12 @@ fun Application.module() {
                     call.receiveMultipart().readAllParts().map {
                         when (it) {
                             is PartData.FormItem -> it.name to it.value
-                            is PartData.FileItem -> it.name to it.originalFileName
+                            is PartData.FileItem -> {
+                                val ext = File(it.originalFileName).extension
+                                val file = File("/data", "upload-${System.currentTimeMillis().hashCode()}.$ext")
+                                it.streamProvider().use { input -> file.outputStream().buffered().use { output -> input.copyToSuspend(output) } }
+                                "" to ""
+                            }
                             else -> {
                                 println("Forbidden PartData")
                                 call.respond(HttpStatusCode.Forbidden)
@@ -192,4 +204,28 @@ object Config {
         return conf.propertyOrNull(key)?.getString() ?: "Aua"
     }
 
+}
+
+suspend fun InputStream.copyToSuspend(
+    out: OutputStream,
+    bufferSize: Int = DEFAULT_BUFFER_SIZE,
+    yieldSize: Int = 4 * 1024 * 1024,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO
+): Long {
+    return withContext(dispatcher) {
+        val buffer = ByteArray(bufferSize)
+        var bytesCopied = 0L
+        var bytesAfterYield = 0L
+        while (true) {
+            val bytes = read(buffer).takeIf { it >= 0 } ?: break
+            out.write(buffer, 0, bytes)
+            if (bytesAfterYield >= yieldSize) {
+                yield()
+                bytesAfterYield %= yieldSize
+            }
+            bytesCopied += bytes
+            bytesAfterYield += bytes
+        }
+        return@withContext bytesCopied
+    }
 }
